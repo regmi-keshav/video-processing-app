80de1e55c70935b74adff22b6a85aff5573abae0
keshav_regmi <keshavregmi70@gmail.com>
2024-09-13 00:20:21 +0530

    Defined Model for our project

--- video_processing_app/videos/models.py
+++ video_processing_app/videos/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    video_file = models.FileField(upload_to='videos/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Subtitle(models.Model):
+    video = models.ForeignKey(
+        Video, on_delete=models.CASCADE, related_name='subtitles')
+    language = models.CharField(max_length=50, default='en')
+    content = models.TextField()
+    # e.g. '00:01:15' for 1 minute, 15 seconds
+    timestamp = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.language} Subtitle for {self.video.title}"
